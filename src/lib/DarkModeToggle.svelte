f6b3cb2b734d3a6f60c123022657753d3f1ad9f7
Tomasz Dudek <tomasz.dudek@netcentric.biz>
2023-03-16 17:17:15 +0100

    removes console log and sets base for index

--- src/lib/DarkModeToggle.svelte
+++ src/lib/DarkModeToggle.svelte
@@ -6,17 +6,14 @@
     let theme = 'light';
 
     const toggleTheme = () => {
-        console.log('click', theme === 'dark'); // todo: remove before merge!
         if (theme === 'dark') {
             theme = 'light';
             localStorage.setItem('theme', 'light');
             document.documentElement.setAttribute('data-theme', 'light');
-            console.log('theme im if 1', theme); // todo: remove before merge!
         } else if (theme === 'light') {
             theme = 'dark';
             localStorage.setItem('theme', 'dark');
             document.documentElement.setAttribute('data-theme', 'dark');
-            console.log('theme im if 1', theme); // todo: remove before merge!
         }
     }
 
@@ -43,7 +40,6 @@
 
     onMount(() => {
         detectColorScheme();
-        console.log('theme', theme); // todo: remove before merge!
     });
 </script>
 
